6831d750b35038996b50e9d4bf44e9c7411d6409
Natalia <hgjmg>
2014-09-27 17:49:36 +0300

    Uploaded files

---
+++ econometrics/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace econometrics
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.zedGraphControl1 = new ZedGraph.ZedGraphControl();
+            this.button2 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(51, 22);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Выбрать файл";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // zedGraphControl1
+            // 
+            this.zedGraphControl1.Location = new System.Drawing.Point(-2, 180);
+            this.zedGraphControl1.Name = "zedGraphControl1";
+            this.zedGraphControl1.ScrollGrace = 0;
+            this.zedGraphControl1.ScrollMaxX = 0;
+            this.zedGraphControl1.ScrollMaxY = 0;
+            this.zedGraphControl1.ScrollMaxY2 = 0;
+            this.zedGraphControl1.ScrollMinX = 0;
+            this.zedGraphControl1.ScrollMinY = 0;
+            this.zedGraphControl1.ScrollMinY2 = 0;
+            this.zedGraphControl1.Size = new System.Drawing.Size(858, 413);
+            this.zedGraphControl1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 59);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(219, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Нарисовать график по данным";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(537, 59);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(127, 21);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(537, 117);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(277, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Построить скользящее среднее";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(687, 59);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(127, 20);
+            this.textBox1.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(544, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Введите тип модели";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(701, 27);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Введите период";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(868, 605);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.zedGraphControl1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Трент";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private ZedGraph.ZedGraphControl zedGraphControl1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ econometrics/Form1.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using ZedGraph;
+
+namespace econometrics
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            comboBox1.Items.Add("Аддитивная");
+            comboBox1.Items.Add("Мультипликативная");    
+        }
+            public List<double> list = new List<double>();
+            public double[] x;
+            public double[] y;
+            public GraphPane myPane;
+            int k;
+            double[] sk;
+        
+         
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string line;
+            
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                StreamReader file =new StreamReader(ofd.FileName);
+                while ((line = file.ReadLine()) != null)
+                {
+                    double d = Double.Parse(line);
+                    list.Add(d);
+                }
+                file.Close();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            myPane = zedGraphControl1.GraphPane;
+            x= new double[list.Count];
+            y = new double[list.Count];
+            for (int i = 0; i < list.Count; i++)
+            {
+                x[i] = i;
+                y[i] = list[i];
+            }
+            zedGraphControl1.GraphPane.CurveList.Clear();
+            PointPairList spl1 = new PointPairList(x, y);
+            LineItem myCurve1 = myPane.AddCurve("Data", spl1, Color.Blue, SymbolType.Diamond);
+            myCurve1.Line.Width = 3.0F;
+            myPane.Title.Text = "***";  
+            zedGraphControl1.AxisChange();
+            zedGraphControl1.Invalidate();
+            zedGraphControl1.Refresh();
+        }
+
+        public int Period
+        {
+            get
+            {
+                if (textBox1.Text == "")
+                {
+                    return 0;
+                }
+                else
+                {
+                    return int.Parse(textBox1.Text);
+                }
+            }
+            set
+            {
+                textBox1.Text = "" + value;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            PointPairList ppl=new PointPairList();
+            PointPair[] spl2 = new PointPair[list.Count - k];
+            if (comboBox1.Text == "Аддитивная")
+            {
+
+                if (Period % 2 == 0)
+                {//четный
+                    k = (Period) / 2;
+                    sk = new double[list.Count - k];
+                    for (int i = k + 1; i < list.Count - k; i++)
+                    {
+                        sk[i] = 0;
+                        for (int j = i - k; j < i + k + 1; j++)
+                        {
+                            sk[i] = y[j]/ (2*Period) + sk[i];
+                            sk[i] = sk[i] ;
+                            spl2[i] = new PointPair(i, sk[i]);
+                            
+                        }
+                        ppl.Add(spl2[i]);
+                    }
+                }
+                else
+                {
+                    k = (Period - 1) / 2;
+                    sk = new double[list.Count - k];
+                    for (int i = k + 1; i < list.Count - k; i++)
+                    {
+                        sk[i] = 0;
+                        for (int j = i - k; j < i + k + 1; j++)
+                        {
+                            sk[i] = y[j]/Period  + sk[i];
+                            spl2[i] = new PointPair(i, sk[i]);
+                        }
+                        ppl.Add(spl2[i]);
+                    }
+
+                }
+            }
+            if (comboBox1.Text == "Мультипликативная")
+            {
+                double g;
+                if (Period % 2 != 0)
+                {
+                    MessageBox.Show(""+y[6]);
+                    k = (Period - 1) / 2;
+                    sk = new double[list.Count - k];
+                    for (int i = k + 1; i < list.Count - k; i++)
+                    {
+                        sk[i] = 0;
+                        for (int j = i - k; j < i + k + 1; j++)
+                        {
+                            g = y[j];
+                            y[j] = Math.Log(y[j]);
+                            if (Double.IsNaN(y[j]))
+                            {
+                                throw new Exception(y[j]+"-"+g);
+                            }
+                            sk[i] = y[j]/ Period + sk[i];
+                            sk[i] = Math.Exp(sk[i]);
+                            spl2[i] = new PointPair(i, sk[i]);
+                        }
+                        ppl.Add(spl2[i]);
+                    }
+                }
+                else
+                {
+                    k = (Period) / 2;
+                    sk = new double[list.Count - k];
+                    for (int i = k + 1; i < list.Count - k; i++)
+                    {
+                        sk[i] = 0;
+                        for (int j = i - k; j < i + k + 1; j++)
+                        {
+                            y[j] = Math.Log(y[j]);
+                            sk[i] = y[j]/ (2 * Period) + sk[i];
+                            sk[i] = Math.Exp(sk[i]);
+                            spl2[i] = new PointPair(i, sk[i]);
+                        }
+                        ppl.Add(spl2[i]);
+                    }
+                }
+            }
+            
+            LineItem myCurve2 = myPane.AddCurve("Skolz", ppl, Color.Red, SymbolType.Diamond);
+            myCurve2.Line.Width = 3.0F;
+            zedGraphControl1.AxisChange();
+            zedGraphControl1.Invalidate();
+            zedGraphControl1.Refresh();
+        }
+    }
+}
